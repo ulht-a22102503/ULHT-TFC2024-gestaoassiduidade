a363c55a3a5b2c31f7b97cc4ab3948aa91cac5df
Daniel Martins <a22102503@alunos.ulht.pt>
2025-06-15 12:17:09 +0100

    Fix models class not found
    Changed order of the classes in models.py

--- management/gestaoassiduidade/models.py
+++ management/gestaoassiduidade/models.py
@@ -3,48 +3,6 @@ from django.db import models
 
 # Create your models here.
 #Base de dados no terminal (MariaDB)
-class Employee(models.Model):
-    id_employee = models.AutoField(db_column='ID_employee', primary_key=True)
-    id_role = models.ForeignKey(JobRole, models.DO_NOTHING, db_column='ID_role')
-    name = models.CharField(max_length=200)
-
-    class Meta:
-        managed = False
-        db_table = 'employee'
-
-
-class Attendance(models.Model):
-    id_attendance = models.IntegerField(db_column='ID_attendance', primary_key=True)
-    id_employee = models.ForeignKey(Employee, db_column='ID_employee', on_delete=models.DO_NOTHING)
-    timestamp = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'attendance'
-
-
-class Fingerprint(models.Model):
-    id_fingerprint = models.IntegerField(primary_key=True, db_column='ID_fingerprint')
-    id_employee = models.ForeignKey(Employee, db_column='ID_employee', on_delete=models.CASCADE)
-    id_sensor_index = models.IntegerField(db_column='ID_sensor_index', blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'fingerprint'
-
-
-class Credentials(models.Model):
-    id_fingerprint = models.AutoField(db_column='ID_fingerprint', primary_key=True)
-    id_employee = models.OneToOneField('Employee', models.DO_NOTHING, db_column='ID_employee')
-    id_sensor_index_main = models.IntegerField(db_column='ID_sensor_index_main', blank=True, null=True)
-    id_sensor_index_sec = models.IntegerField(db_column='ID_sensor_index_sec', blank=True, null=True)
-    pincode = models.CharField(max_length=64, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'credentials'
-
-
 class JobRole(models.Model):
     id_role = models.CharField(db_column='ID_role', primary_key=True, max_length=4)
     descript = models.CharField(max_length=50)
@@ -86,3 +44,45 @@ class Workcode(models.Model):
     class Meta:
         managed = False
         db_table = 'workcode'
+
+
+class Employee(models.Model):
+    id_employee = models.AutoField(db_column='ID_employee', primary_key=True)
+    id_role = models.ForeignKey(JobRole, models.DO_NOTHING, db_column='ID_role')
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'employee'
+
+
+class Attendance(models.Model):
+    id_attendance = models.IntegerField(db_column='ID_attendance', primary_key=True)
+    id_employee = models.ForeignKey(Employee, db_column='ID_employee', on_delete=models.DO_NOTHING)
+    timestamp = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'attendance'
+
+
+class Fingerprint(models.Model):
+    id_fingerprint = models.IntegerField(primary_key=True, db_column='ID_fingerprint')
+    id_employee = models.ForeignKey(Employee, db_column='ID_employee', on_delete=models.CASCADE)
+    id_sensor_index = models.IntegerField(db_column='ID_sensor_index', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'fingerprint'
+
+
+class Credentials(models.Model):
+    id_fingerprint = models.AutoField(db_column='ID_fingerprint', primary_key=True)
+    id_employee = models.OneToOneField('Employee', models.DO_NOTHING, db_column='ID_employee')
+    id_sensor_index_main = models.IntegerField(db_column='ID_sensor_index_main', blank=True, null=True)
+    id_sensor_index_sec = models.IntegerField(db_column='ID_sensor_index_sec', blank=True, null=True)
+    pincode = models.CharField(max_length=64, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'credentials'
